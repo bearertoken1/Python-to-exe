c48f27bd1ff8a0c0fb6139f736d9cfd68b4659fa
bearertoken <wardentech123@gmail.com>
2025-12-06 05:48:40 +0000

    Add userscript for automatic alert on repo visit

---
+++ github-alert.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Python-to-exe Alert
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Show alert on Python-to-exe repo
+// @author       bearertoken1
+// @match        https://github.com/bearertoken1/Python-to-exe*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert("hi bearert0ken");
+})();
